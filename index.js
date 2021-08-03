d795705f0ae231f1dd00c21719b32e7d29b080fb
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-08-02 21:56:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record) {
+  const winningSeason = record.find(season => season.result == "W")
+  if (winningSeason) {
+    return winningSeason.year
+  } else {
+    return undefined
+  }
+}
